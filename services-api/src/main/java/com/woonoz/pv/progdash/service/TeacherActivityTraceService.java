a39a8a06f678cde6135e41e912956fa40daf654e
Marine Sivade <marine.sivade@woonoz.com>
2019-02-15 17:44:24 +0100

    [TECH-463] Migration du code back avec mybatis, spring boot, jersey et mongodb dans le dossier back

--- services-api/src/main/java/com/woonoz/pv/progdash/service/TeacherActivityTraceService.java
+++
@@ -1,8 +0,0 @@
-package com.woonoz.pv.progdash.service;
-
-import com.woonoz.pv.progdash.dto.TeacherActivityTraceDto;
-
-public interface TeacherActivityTraceService {
-
-	void saveTeacherActivityTrace(TeacherActivityTraceDto teacherActivityTraceDto);
-}
