48e99d6c40c0a9a21491cc91a5dcbe5570eeba73
Marine Sivade <marine.sivade@woonoz.com>
2019-02-15 17:44:24 +0100

    [TECH-463] Migration du code back avec mybatis, spring boot, jersey et mongodb dans le dossier back

--- services-api/src/main/java/com/woonoz/pv/progdash/service/LearningSessionStatisticsService.java
+++
@@ -1,8 +0,0 @@
-package com.woonoz.pv.progdash.service;
-
-import com.woonoz.pv.progdash.dto.LearningSessionStatisticsDto;
-
-public interface LearningSessionStatisticsService {
-
-	LearningSessionStatisticsDto getLearningSessionStatistics(Integer userId, String message);
-}
