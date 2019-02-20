95518536f281cf630542d157c64cab7e972c446a
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-02-20 12:07:22 +0100

    [TECH-481] ajout d'un build maven pour mohamed

--- docker/dev.java.build.sh
+++ docker/dev.java.build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(cd `dirname $0` && pwd)
+find $SCRIPT_PATH/../back/webservices/target -name webservices-*-spring-boot.jar| xargs -I % cp % $SCRIPT_PATH/jar/webservices.jar
+
 export DOCKER_TAG=latest
 docker-compose --project-directory ../. --file docker-compose.yml --file docker-compose.java.yml build
