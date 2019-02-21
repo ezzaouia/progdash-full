9d4095822c5370408f65af52f02e37cf1cc4c9bc
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-02-21 10:49:02 +0100

    [TECH-482] build jenkins

--- docker/scripts/cronjobs/backup-mongodb.sh
+++ docker/scripts/cronjobs/backup-mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DUMP_FILENAME="/data/backups/dump_mongodb_$(date +\%Y\%m\%d-\%H\%M).sql.gz"
+DUMP_FILENAME="/data/backups/dump_mongodb_$(date +\%Y\%m\%d-\%H\%M).gz"
 
 echo ":: Dump mongodb"
-mongodump --host mongodb --username ${MONGO_USER} --password ${MONGO_PASS} --gzip --archive=$DUMP_FILENAME
+/mongo-tools/mongodump --host mongodb --db ${MONGO_PROGDASH_DB} --username ${MONGO_PROGDASH_USER} --password ${MONGO_PROGDASH_PASS} --gzip --archive=${DUMP_FILENAME}
 
 exit 0
