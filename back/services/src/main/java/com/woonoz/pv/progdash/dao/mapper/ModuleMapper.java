5dd88cd3057e2932036fcace9cc897a5cd26d0fb
a.crouau <alan.crouau@woonoz.com>
2019-02-21 17:09:03 +0100

    [PVCV-1136] ajoute getModuleInfos

--- back/services/src/main/java/com/woonoz/pv/progdash/dao/mapper/ModuleMapper.java
+++ back/services/src/main/java/com/woonoz/pv/progdash/dao/mapper/ModuleMapper.java
@@ -1,9 +1,13 @@
 package com.woonoz.pv.progdash.dao.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.woonoz.pv.progdash.dao.dbo.ModuleInfoDbo;
 
 @Mapper
 public interface ModuleMapper {
-
-
+	List<ModuleInfoDbo> getModulesInfo(@Param("areaId")int areaId);
 }
