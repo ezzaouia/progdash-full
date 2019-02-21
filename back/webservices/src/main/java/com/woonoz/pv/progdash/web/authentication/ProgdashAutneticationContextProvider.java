1f893b2dee52a3cebb6345f0a8198feb6f081a42
Marine Sivade <marine.sivade@woonoz.com>
2019-02-21 16:32:50 +0100

    [TECH-466] Ajout du wrapper pour récupérer l'utilisateur authentifié

---
+++ back/webservices/src/main/java/com/woonoz/pv/progdash/web/authentication/ProgdashAutneticationContextProvider.java
@@ -0,0 +1,18 @@
+package com.woonoz.pv.progdash.web.authentication;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Component;
+
+import com.woonoz.auth.services.AuthenticationService;
+import com.woonoz.pv.progdash.model.ProgdashUserDetails;
+
+@Component
+public class ProgdashAutneticationContextProvider {
+
+	@Inject private AuthenticationService authenticationService;
+
+	public ProgdashUserDetails getAuthenticatedUser(){
+		return (ProgdashUserDetails) authenticationService.getAuthenticatedUser();
+	}
+}
