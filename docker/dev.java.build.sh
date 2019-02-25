edab833c6622b442b1cfa700e6dc75addb0e55b9
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-02-25 10:51:24 +0100

    [TECH-461] correction find jar webservices

--- docker/dev.java.build.sh
+++ docker/dev.java.build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
-if [ -d "$SCRIPT_PATH/docker/jar" ]; then
-    rm -rf $SCRIPT_PATH/docker/jar/*
+if [ -d "$SCRIPT_PATH/jar" ]; then
+    rm -rf $SCRIPT_PATH/jar/*
 else
-    mkdir -p $SCRIPT_PATH/docker/jar
+    mkdir -p $SCRIPT_PATH/jar
 fi
 find $SCRIPT_PATH/../back/webservices/target -name webservices-*-spring-boot.jar| xargs -I % cp % $SCRIPT_PATH/jar/webservices.jar
 
