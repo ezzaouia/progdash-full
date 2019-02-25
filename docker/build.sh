db288c488a7caa226ea433a5e27adf44daba9dd2
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-02-25 10:45:08 +0100

    [TECH-461] correction find jar webservices

--- docker/build.sh
+++ docker/build.sh
@@ -11,6 +11,11 @@ then
 fi
 
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
+if [ -d "$SCRIPT_PATH/docker/jar" ]; then
+    rm -rf $SCRIPT_PATH/docker/jar/*
+else
+    mkdir -p $SCRIPT_PATH/docker/jar
+fi
 find $SCRIPT_PATH/../back/webservices/target -name webservices-*-spring-boot.jar| xargs -I % cp % $SCRIPT_PATH/jar/webservices.jar
 
 echo "DOCKER_TAG: $DOCKER_TAG"
--- docker/dev.java.build.sh
+++ docker/dev.java.build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
+if [ -d "$SCRIPT_PATH/docker/jar" ]; then
+    rm -rf $SCRIPT_PATH/docker/jar/*
+else
+    mkdir -p $SCRIPT_PATH/docker/jar
+fi
 find $SCRIPT_PATH/../back/webservices/target -name webservices-*-spring-boot.jar| xargs -I % cp % $SCRIPT_PATH/jar/webservices.jar
 
 export DOCKER_TAG=latest
