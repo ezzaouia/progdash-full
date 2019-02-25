e5d7455fcf7b861d2bda49708a0e8492f53950a3
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-02-25 10:23:04 +0100

    [TECH-461] correction find jar webservices

--- docker/build.sh
+++ docker/build.sh
@@ -10,6 +10,8 @@ then
     export DOCKER_TAG=latest
 fi
 
-echo "DOCKER_TAG: $DOCKER_TAG"
+SCRIPT_PATH=$(cd `dirname $0` && pwd)
+find $SCRIPT_PATH/../back/webservices/target -name webservices-*-spring-boot.jar| xargs -I % cp % $SCRIPT_PATH/jar/webservices.jar
 
+echo "DOCKER_TAG: $DOCKER_TAG"
 docker-compose --project-directory ../. --file docker-compose.yml --file docker-compose.packaging.yml build --parallel
