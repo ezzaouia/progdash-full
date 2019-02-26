786a589c059fc8cb1d3659b8e1d39e57006e8672
Mohamed Ezzaouia <mohamed@speakplus.fr>
2019-02-26 09:23:39 +0100

    [PVCV-1135] adding missing env variables

--- front/src/environments/environment.hmr.ts
+++ front/src/environments/environment.hmr.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: false,
   loglevel: 4,
   hmr: true,
+  apiUrl: '/suivi/api',
+  pvApiUrl: '/services-pjv',
   API_URL: 'http://localhost:4200/api',
   SUIVI_STATS_URL: 'https://statistiques.projet-voltaire.fr',
  };
