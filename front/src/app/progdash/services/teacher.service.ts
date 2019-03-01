a741bcdf407506ff5bedc170dc63f80b9ae52f09
Mohamed Ezzaouia <mohamed@speakplus.fr>
2019-03-01 16:36:43 +0100

    [PVCV-1139] hot fix request body format

--- front/src/app/progdash/services/teacher.service.ts
+++ front/src/app/progdash/services/teacher.service.ts
@@ -2,42 +2,35 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { join, map as mapl } from 'lodash';
 
 const API_URL = environment.SUIVI_STATS_URL;
 @Injectable()
 export class TeacherService {
+  constructor ( private http: HttpClient ) {}
 
-    constructor ( private http: HttpClient ) {
-    }
+  public getLink ({ lessons, userInfo }) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }),
+    };
 
-    public getLink ({ lessons, userInfo }) {
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'X-Requested-With':  'XMLHttpRequest',
-              'Content-Type': 'application/x-www-form-urlencoded',
-            }),
-        };
+    const lessonsMap = mapl( lessons, l => `chapters=${l}` );
+    const url = join( lessonsMap, '&' );
 
-        // const body = new HttpParams();
-        const formData = new FormData();
+    const pathApi = API_URL +
+        `/services-pjv/rest/manager/area/${userInfo.areaId}/teacher/module/url`;
 
-        for ( const l of lessons ) {
-          formData.append( 'chapters[]',  l );
-        }
-
-        const pathApi = API_URL +
-            `/services-pjv/rest/manager/area/${userInfo.areaId}/teacher/module/url`;
-
-        return this.http
-            .post<{status: String, data: String}>( pathApi, formData )
-            .pipe(
-                map( response => {
-                    if ( response.status === 'error' ) {
-                        throw new Error( 'impossible d\'obtenir le mode présentiel' );
-                    }
-                    return response.data;
-                })
-            );
-    }
-
+    return this.http
+      .post<{ status: String; data: String }>( pathApi, url, httpOptions )
+      .pipe(
+        map( response => {
+            if ( response.status === 'error' ) {
+            throw new Error( 'impossible d\'obtenir le mode présentiel' );
+            }
+            return response.data;
+        }));
+  }
 }
+
