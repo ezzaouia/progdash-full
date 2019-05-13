58ad5889f255474a2ece81c644e0ea6470e4fbca
Thomas Nerrant <thomas.nerrant@woonoz.com>
2019-05-13 17:08:16 +0200

    [TECH-587] nettoyage backup

--- docker/scripts/cronjobs/backup-mongodb.sh
+++ docker/scripts/cronjobs/backup-mongodb.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-DUMP_FILENAME="/data/backups/dump_mongodb_$(date +\%Y\%m\%d-\%H\%M).gz"
+BACKUP_PATH="/data/backups"
+DUMP_FILENAME="dump_mongodb_$(date +\%Y\%m\%d-\%H\%M).gz"
+cd ${BACKUP_PATH}
 
 echo ":: Dump mongodb"
-/mongo-tools/mongodump --host mongodb --db ${MONGO_PROGDASH_DB} --username ${MONGO_PROGDASH_USER} --password ${MONGO_PROGDASH_PASS} --gzip --archive=${DUMP_FILENAME}
+/mongo-tools/mongodump --host mongodb --db ${MONGO_PROGDASH_DB} --username ${MONGO_PROGDASH_USER} --password ${MONGO_PROGDASH_PASS} --gzip --archive=${BACKUP_PATH}/${DUMP_FILENAME}
+
+echo ":: clean older backup"
+if [ -f ${BACKUP_PATH}/${DUMP_FILENAME} ];
+then
+    find ${BACKUP_PATH} ! -name "${DUMP_FILENAME}" -type f -exec rm -f {} \;
+fi
 
 exit 0
